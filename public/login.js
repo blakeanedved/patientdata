304ed7003396f089b340403fd5e85e13c9f2f3fd
blakeanedved <blakeanedved@gmail.com>
2019-10-05 13:36:20 -0500

    Added login page

---
+++ public/login.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+	firebase.auth().signOut()
+
+	firebase.auth().onAuthStateChanged(user => {
+		if (user) window.location = '/data.html'
+	})
+
+	function submit() {
+		var email = $('#email').val()
+		var password = $('#password').val()
+		if (/([A-z0-9])+@([A-z0-9])+\.([a-z]){2,}/.test(email)) {
+			if (password.length >= 8) {
+				firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+					console.log(error);
+				});
+			}
+		}
+	}
+
+	$('#submit').click(submit)
+
+	document.addEventListener('keydown', e => {
+		if (e.keyCode == 13) submit();
+	})
+})
